1d0a8ae3dba8a23cbb24db27376fa16d0cbff6cc
birdloza <birdloza@gmail.com>
2024-05-07 23:34:32 +0400

    CAR-6 added jscode

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     })
 
+let slider = document.querySelectorAll('.slider');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let imageIndex = 0;
+
+next.onclick = function () {
+    
+    slider[imageIndex].classList.remove('active');
+    if (imageIndex === slider.length - 1) {
+        imageIndex = 0;
+    } else {
+    imageIndex += 1;
+};
+slider[imageIndex].classList.add('active');
+};
+prev.onclick = function () {
+    slider[imageIndex].classList.remove('active');
+    if (imageIndex === 0) {
+        imageIndex = slider.length - 1;
+    } else {
+        imageIndex -= 1;
+    };
+    slider[imageIndex].classList.add('active');
+};
